785c0d761b0f215e37816296e32525299f9893a4
MatthiasOoms <matthias.ooms1@gmail.com>
2024-04-17 12:51:44 +0200

    Tested some extra code with RemoveComponent

--- Minigin/GameObject.h
+++ Minigin/GameObject.h
@@ -90,7 +90,7 @@ namespace dae
 	template<typename Comp>
 	inline void GameObject::RemoveComponent()
 	{
-		Comp* temp;
+		Comp* temp{};
 		for (size_t idx{}; idx < m_pComponents.size(); idx++)
 		{
 			temp = dynamic_cast<Comp*>(m_pComponents[idx]);
@@ -103,8 +103,6 @@ namespace dae
 				m_pComponents.erase(nth);
 			}
 		}
-		delete temp;
-		temp = nullptr;
 	}
 
 	template<typename Comp>
